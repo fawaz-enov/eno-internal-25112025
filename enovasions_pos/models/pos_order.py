7807b7d9fa0920779036e91e395e3dea9b8f2eaa
fawaz-enov <fawaz.zuhair@enovasions.com>
2026-01-03 05:41:22 +1200

    Update pos_order.py

--- enovasions_pos/models/pos_order.py
+++ enovasions_pos/models/pos_order.py
@@ -312,7 +312,7 @@ class AccountMoveInherit(models.Model):
                         'invoiceType': invoiceType,
                         'transactionType': transactionType,
                         'payment': [],
-                        'invoiceNumber': record.name,
+                        'invoiceNumber': '32/2.01',
                         'referentDocumentNumber': '',
                         'referentDocumentDT': '',
                         'items': []
@@ -700,7 +700,7 @@ class AccountMoveInherit(models.Model):
                         'invoiceType': invoiceType,
                         'transactionType': transactionType,
                         'payment': [],
-                        'invoiceNumber': record.name,
+                        'invoiceNumber': '32/2.01',
                         'referentDocumentNumber': record.ref_doc_num,
                         'referentDocumentDT': record.ref_doc_date.strftime('%Y-%m-%d %H:%M:%S') if record.ref_doc_date else '',
                         'items': []
